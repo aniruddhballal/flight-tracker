3becf35a8c4accd296da870402dd35a6a381f936
aniruddhballal <aniruddhballaldeeksha@gmail.com>
2025-11-28 00:23:36 +0530

    fix: invert north/south on Android compass

--- src/FlightMap.tsx
+++ src/FlightMap.tsx
@@ -132,8 +132,8 @@ const FlightMap: React.FC<FlightMapProps> = ({ flights, cityName, cityCoords }) 
       // iOS
       heading = webkitEvent.webkitCompassHeading;
     } else if (event.alpha !== null) {
-      // Android - rotate 90 degrees clockwise
-      heading = (event.alpha + 90) % 360;
+      // Android - rotate 90° clockwise, then mirror north/south
+      heading = (270 - event.alpha + 360) % 360;
     }
 
     currentHeadingRef.current = heading;
